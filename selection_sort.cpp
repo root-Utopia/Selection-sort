aedb8b51ab147bec90ac1bc5ef4669f55f5b6f11
Arkadiy <mobilehelp15@gmail.com>
2020-02-08 21:14:27 +0200

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+void selection_sort(int* arr, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		int min_idx = i, min_val = arr[min_idx];
+		for (int j = i; j < n; ++j)
+			if (min_val > arr[j])
+				min_idx = j,
+				min_val = arr[j];
+
+		swap(arr[i], arr[min_idx]);
+	}
+}
